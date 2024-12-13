22b3ba72061f7c387f9e8b9b3ff2fc6ac9e26bd7
Tom Harris <tkmharris@protonmail.com>
2024-12-13 21:47:57 +0000

    Tidy day 13

--- solutions/day13.py
+++ solutions/day13.py
@@ -16,64 +16,70 @@ from utils.solver import Solver
 from utils.input import read_input
 import re
 
-def cost(A: int, B: int) -> int:
+def _cost(A: int, B: int) -> int:
     return 3 * A + B
 
 
-def is_nonnegative_integral(number: float, epsilon: float = 1e-9) -> bool:
-    return abs(number - round(number, 0)) < epsilon and number > -epsilon
-
-
-def solve_simultaneous_equations(
-        a_x: int, b_x: int, prize_x: int, # represents a_x * A + b_x * B = prize_x
-        a_y: int, b_y: int, prize_y: int  # represents a_y * A + b_y * B = prize_y
-) -> tuple[float, float]:
+def _solve_simultaneous_equations_in_nonnegative_integers(
+        # represents a_x * A + b_x * B = prize_x
+        a_x: int, b_x: int, prize_x: int,
+        # represents a_y * A + b_y * B = prize_y
+        a_y: int, b_y: int, prize_y: int 
+) -> tuple[int, int] | None:
     det = a_x * b_y - a_y * b_x
     # Luckily for us, all the equations in the input have unique solutions.
     assert(det) != 0
 
     # formula falls out of the equations after inverting the 2x2 matrix
-    solution = (
-        (b_y * prize_x - b_x * prize_y) / det,
-        (a_x * prize_y - a_y * prize_x) / det
+    # we check for divisibility by det first so we don't have to cast to floots
+    # and handle precision issues
+    A, B = (
+        (b_y * prize_x - b_x * prize_y),
+        (a_x * prize_y - a_y * prize_x)
     )
-    return solution
+    if A % det != 0 or B % det != 0:
+        return None
+    
+    A, B = A // det, B // det
+    if A < 0 or B < 0:
+        return None
 
+    return A, B
+
+def _parse_input_lines(lines: list[str]) -> list[tuple[int, int, int, int, int, int]]:
+    input_variables = []
+    current_inputs = [0] * 6
+    for i, line in enumerate(lines):
+        if not line.strip('\n'):
+            continue
+        x, y = map(int, re.findall(r'\d+', line))
+        
+        if i % 4 == 0:
+            current_inputs[0], current_inputs[1] = x, y
+        elif i % 4 == 1:
+            current_inputs[2], current_inputs[3] = x, y
+        elif i % 4 == 2:
+            current_inputs[4], current_inputs[5] = x, y
+            input_variables.append(tuple(current_inputs))
+        else:
+            continue
+    return input_variables
 
 class Day13(Solver):
-    def parse_input_lines(self, lines: list[str]) -> list[tuple[int, int, int, int, int, int]]:
-        input_variables = []
-        current_inputs = [0] * 6
-        for i, line in enumerate(lines):
-            if not line.strip('\n'):
-                continue
-            x, y = map(int, re.findall(r'\d+', line))
-            
-            if i % 4 == 0:
-                current_inputs[0], current_inputs[1] = x, y
-            elif i % 4 == 1:
-                current_inputs[2], current_inputs[3] = x, y
-            elif i % 4 == 2:
-                current_inputs[4], current_inputs[5] = x, y
-                input_variables.append(tuple(current_inputs))
-            else:
-                continue
-        return input_variables
-    
     @read_input("lines")
     def part1(self, lines: list[str]) -> int: 
         total_cost = 0
         
-        for input_variables in self.parse_input_lines(lines):
+        for input_variables in _parse_input_lines(lines):
             a_x, a_y, b_x, b_y, prize_x, prize_y = input_variables
             
-            A, B = solve_simultaneous_equations(
+            solution = _solve_simultaneous_equations_in_nonnegative_integers(
                 a_x, b_x, prize_x,
                 a_y, b_y, prize_y
             )
-            if is_nonnegative_integral(A) and is_nonnegative_integral(B):
-                A, B = int(round(A, 0)), int(round(B, 0))
-                total_cost += cost(A, B)
+            if solution:
+                A, B = solution
+                total_cost += _cost(A, B)
         
         return total_cost
 
@@ -81,17 +87,17 @@ class Day13(Solver):
     def part2(self, lines: list[str]) -> int: 
         total_cost = 0
         
-        for input_variables in self.parse_input_lines(lines):
+        for input_variables in _parse_input_lines(lines):
             a_x, a_y, b_x, b_y, prize_x, prize_y = input_variables
             prize_x, prize_y = 10000000000000 + prize_x, 10000000000000 + prize_y
              
-            A, B = solve_simultaneous_equations(
+            solution = _solve_simultaneous_equations_in_nonnegative_integers(
                 a_x, b_x, prize_x,
                 a_y, b_y, prize_y
             )
-            if is_nonnegative_integral(A) and is_nonnegative_integral(B):
-                A, B = int(round(A, 0)), int(round(B, 0))
-                total_cost += cost(A, B)
+            if solution:
+                A, B = solution
+                total_cost += _cost(A, B)
 
         return total_cost
 
