c74f3eb991fad4b15c1cd3dc15570cea2374511a
Tom Harris <tkmharris@protonmail.com>
2024-12-15 00:04:36 +0000

    Make day 14 yield a return value.

--- solutions/day14.py
+++ solutions/day14.py
@@ -1,12 +1,3 @@
-"""
-NOTES:
-I'm not really happy with this. The chi-squared test is a bit ad hoc
-and even with a high threshold I still have to page through quite a lot
-of configurations manually to find the Christmas tree. Maybe it would be
-better to just look at the variance? Keep an ongoing average of the variance 
-so far and stop if it differs by a lot?
-"""
-
 from utils.solver import Solver
 from utils.input import read_input
 from dataclasses import dataclass
@@ -27,12 +18,14 @@ class Grid:
         LOWER_LEFT = "lower_left"
         LOWER_RIGHT = "lower_right"
 
+
 @dataclass
 class Robot:
     position_x: int
     position_y: int
     velocity_x: int
     velocity_y: int
+
 
     def move(self, steps: int, grid: Grid):
         self.position_x = (self.position_x + steps * self.velocity_x) % grid.width
@@ -53,7 +46,7 @@ class Robot:
                 return Grid.Quadrant.LOWER_RIGHT
 
 
-def show(grid: Grid, robots: list[Robot]):
+def display(grid: Grid, robots: list[Robot]):
     robots_by_square = defaultdict(int)
     for robot in robots:
         robots_by_square[(robot.position_x, robot.position_y)] += 1
@@ -70,26 +63,17 @@ def show(grid: Grid, robots: list[Robot]):
     print('\n'.join(lines))
 
 
-def chi_squared_test(
-        actuals: list[int], 
-        expecteds: list[int | float]
-    ) -> bool:
+def variance(points: list[tuple[int, int]]) -> float:
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
 
-    # No science to this, just pick a threshold high enough that paging through
-    # configurations goes quickly without stopping at too many false positives.
-    CHI_SQUARED_THRESHOLD = 50
-    
-    if len(actuals) != len(expecteds):
-        raise ValueError("Actual values must be same length as expected")
-    statistic = sum(
-        (actual - expected)**2 / expected
-        for actual, expected in zip(actuals, expecteds)
-    )
+    x_bar = sum(xs) / len(xs)
+    y_bar = sum(ys) / len(ys)
 
-    if statistic > CHI_SQUARED_THRESHOLD:
-        return False
-    
-    return True
+    return sum(
+        (point[0] - x_bar)**2 + (point[1] - y_bar)**2
+        for point in points
+    ) / len(points)
 
 
 class Day14(Solver):
@@ -97,20 +81,25 @@ class Day14(Solver):
         super().__init__(input_file)
         self.grid = grid
     
+
     def get_robots(self, lines: list[str]) -> list[Robot]:
         return [
             Robot(*tuple(map(int, re.findall(r'(\d+|\-\d+)', line))))
             for line in lines
         ]
+    
+
+    def move_robots(self, robots: list[Robot], steps=1):
+        for robot in robots:
+            robot.move(steps=steps, grid=self.grid)
 
 
     @read_input("lines")
     def part1(self, lines: list[str]) -> int:
         robots = self.get_robots(lines)
 
-        for robot in robots:
-            robot.move(steps=100, grid=self.grid)
-
+        self.move_robots(robots, steps=100)
+        
         robots_by_quadrant = defaultdict(list)
         for robot in robots:
             if quadrant := robot.quadrant(self.grid):
@@ -122,43 +111,38 @@ class Day14(Solver):
 
 
     @read_input("lines")
-    def part2(self, lines: list[str]):
+    def part2(self, lines: list[str], show: bool = False):
         robots = self.get_robots(lines)
-        expected_count = len(robots) * (50 * 51) / (101 * 103)
-
-        def get_next(initial=False) -> bool:
-            if initial:
-                input_string = 'Press Enter to begin search for Christmas tree or type "exit" to stop: '
-            else:
-                input_string = 'Press Enter to continue or type "exit" to stop: '
-            user_input = input(input_string)
-            if user_input.lower() == "exit":
-                return False
-            return True
         
-        generation = 0
-        get_next()
-        while True:
-            generation += 1
-            for robot in robots:
-                robot.move(steps=1, grid=self.grid)
-
-            robots_by_quadrant = defaultdict(list)
-            for robot in robots:
-                if quadrant := robot.quadrant(self.grid):
-                    robots_by_quadrant[quadrant].append(robot)
+        def _get_robot_position_variance(robots: list[Robot]) -> float:
+            return variance([
+                (robot.position_x, robot.position_y)
+                for robot in robots
+            ])
+        
+        minimum_variance: float | None = None
+        best_time: int = 0
+        # velocities are constant so configuration repeats after at most
+        # grid.width * grid.height iterations
+        for seconds in range(self.grid.width * self.grid.height):
+            robot_variance = _get_robot_position_variance(robots)
             
-            actual_counts = [
-                int(len(robots)) for _, robots in robots_by_quadrant.items()
-            ]
-            if not chi_squared_test(actual_counts, [expected_count] * 4):
-                show(self.grid, robots)
-                print(generation)
-                if not get_next():
-                    exit(0)
+            if minimum_variance is None or robot_variance < minimum_variance:
+                minimum_variance = robot_variance
+                best_time = seconds
+            
+            self.move_robots(robots)
+            
+        if show:
+            # robots are back in their initial configuration after
+            # grid.width * grid.height steps
+            self.move_robots(robots, steps=best_time)
+            display(self.grid, robots)
+        
+        return best_time
     
 
 if __name__=="__main__":
     solver = Day14("input14.txt", grid=Grid(101, 103))
-    print(solver.part1())
-    print(solver.part2())
+    print(solver.part1()) # type: ignore
+    print(solver.part2()) # type: ignore
